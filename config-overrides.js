2d045ea734fe342a25ef3e012276381b8f8cbba1
Lang <1584731441@qq.com>
2025-02-13 17:07:04 +0800

    init

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const {
+  override,
+  addWebpackAlias,
+} = require('customize-cra');
+
+module.exports = {
+  webpack: override(
+    addWebpackAlias({
+      '@': path.resolve(__dirname, 'src'),
+    }),
+  ),
+};
+
